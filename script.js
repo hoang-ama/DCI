348e3d58b68506a82183765fcf236b213e6658c3
Minh Hoang <minhhh@Minhs-MacBook-Air.local>
2025-10-03 09:23:04 +0700

    1st commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript code here to create interactive effects,
+// for example: smooth scrolling, a mobile menu, or animations.
+
+// Example: Display a simple message when the page loads
+console.log("The DCI website is ready!");
